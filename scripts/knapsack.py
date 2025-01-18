92d7ca0f788b693ba5aa913169ce7e06a2dd9f12
AVINABA GHOSH HAJRA <avinaba.ghoshhajra@tcs.com>
2025-01-19 04:19:40 +0530

    Updated python script

---
+++ scripts/knapsack.py
@@ -0,0 +1,23 @@
+# Returns the maximum value that
+# can be put in a knapsack of capacity W
+def knapSack(W, wt, val):
+    n = len(wt)
+    dp = [[0 for _ in range(W + 1)] for _ in range(n + 1)]
+
+    # Build table dp[][] in bottom-up manner
+    for i in range(n + 1):
+        for w in range(W + 1):
+            if i == 0 or w == 0:
+                dp[i][w] = 0
+            elif wt[i - 1] <= w:
+                dp[i][w] = max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w])
+            else:
+                dp[i][w] = dp[i - 1][w]
+    return dp[n][W]
+
+# Driver Code
+profit = [60, 100, 120]
+weight = [10, 20, 30]
+W = 50
+
+print(knapSack(W, weight, profit))
